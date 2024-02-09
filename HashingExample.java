ff24b01343fed8128c323543816ff6f854444858
Tausif Ahmad <tausifahmad754@gmail.com>
2024-02-09 23:19:42 +0530

    HashMap example

---
+++ HashingExample.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class HashingExample {
+    public static void main(String[] args) {
+        System.out.println("welcome");
+    
+    HashMap<String, Integer> map=new HashMap<>();
+    //insertion
+    map.put("Tausif",47);
+    System.out.println(map);
+
+
+    }
+    
+}
