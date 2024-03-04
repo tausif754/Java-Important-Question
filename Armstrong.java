3e1e3854a6649207149bc1545fb9986e40689e78
Tausif Ahmad <tausifahmad754@gmail.com>
2024-03-04 23:59:25 +0530

    Armstrong number

--- Armstrong.java
+++ Armstrong.java
@@ -2,12 +2,23 @@ public class Armstrong {
     static boolean isArmstrong(int n){
         //Calculate the number of digits
         int numberOfDigits=String.valueOf(n).length();
-        System.out.println(numberOfDigits);
-        return true;
+        int originalNumber=n;
+        int sum=0;
+        while(n>0){
+            int digits=n%10;
+            sum+=Math.pow(digits, numberOfDigits);
+            n=n/10;
+        }
+        return sum==originalNumber;
 
     }
     public static void main(String[] args) {
-        isArmstrong(460088);
+        int numToCheck=153;
+        if(isArmstrong(numToCheck)){
+            System.out.println(numToCheck +  "is an Armstrong number");
+        }else{
+            System.out.println(numToCheck  + "is Not an Armstromg number");
+        }
     }
     
 }
